da4b896f326bf8a528fa582ee6d7ad71480c99d1
MLOps 2025 <MLOps2025@outlook.com>
2024-12-27 09:58:45 +0530

    fixed AB#4

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
 
-    <h1>Gen Ai & AI Agents</h1>
+    <h1>Gen Ai & AI Agents - Title Changes</h1>
 	<h2>Upload your data to get Gen Ai predictions - Modified</h2>
     <img
       alt="Vue logo"
